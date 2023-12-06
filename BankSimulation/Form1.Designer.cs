6d4741834b2ec89fc401684b52111e079d7114c6
Lixxxuan <1366070554@qq.com>
2023-12-06 10:50:17 +0800

    添加项目文件。

---
+++ BankSimulation/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace BankSimulation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTicket = new System.Windows.Forms.Label();
+            this.lblServing = new System.Windows.Forms.Label();
+            this.btnCustomer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTicket
+            // 
+            this.lblTicket.AutoEllipsis = true;
+            this.lblTicket.AutoSize = true;
+            this.lblTicket.Location = new System.Drawing.Point(263, 321);
+            this.lblTicket.Name = "lblTicket";
+            this.lblTicket.Size = new System.Drawing.Size(62, 18);
+            this.lblTicket.TabIndex = 0;
+            this.lblTicket.Text = "label1";
+            // 
+            // lblServing
+            // 
+            this.lblServing.AutoSize = true;
+            this.lblServing.Location = new System.Drawing.Point(463, 321);
+            this.lblServing.Name = "lblServing";
+            this.lblServing.Size = new System.Drawing.Size(62, 18);
+            this.lblServing.TabIndex = 1;
+            this.lblServing.Text = "label2";
+            // 
+            // btnCustomer
+            // 
+            this.btnCustomer.Location = new System.Drawing.Point(257, 110);
+            this.btnCustomer.Name = "btnCustomer";
+            this.btnCustomer.Size = new System.Drawing.Size(284, 98);
+            this.btnCustomer.TabIndex = 2;
+            this.btnCustomer.Text = "button1";
+            this.btnCustomer.UseVisualStyleBackColor = true;
+            this.btnCustomer.Click += new System.EventHandler(this.btnCustomer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 481);
+            this.Controls.Add(this.btnCustomer);
+            this.Controls.Add(this.lblServing);
+            this.Controls.Add(this.lblTicket);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTicket;
+        private System.Windows.Forms.Label lblServing;
+        private System.Windows.Forms.Button btnCustomer;
+    }
+}
+
---
+++ BankSimulation/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BankSimulation
+{
+    public partial class Form1 : Form
+    {
+        private SemaphoreSlim semaphore; // 信号量，用于控制顾客的访问
+        private Queue<int> queue; // 顾客队列，存储顾客的号码
+
+        reasonml
+        复制
+        public Form1()
+        {
+            InitializeComponent();
+            semaphore = new SemaphoreSlim(1); // 初始化信号量，初始计数为1
+            queue = new Queue<int>();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 创建服务窗口和座位
+            CreateServiceWindow();
+            CreateSeats();
+        }
+
+        private void CreateServiceWindow()
+        {
+            Label lblServiceWindow = new Label();
+            lblServiceWindow.Text = "服务窗口";
+            lblServiceWindow.AutoSize = true;
+            lblServiceWindow.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            lblServiceWindow.Location = new System.Drawing.Point(20, 20);
+            Controls.Add(lblTicket);
+            Controls.Add(lblServiceWindow);
+        }
+
+        private void CreateSeats()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Label lblSeat = new Label();
+                lblSeat.Text = "座位 " + (i + 1);
+                lblSeat.AutoSize = true;
+                lblSeat.Location = new System.Drawing.Point(20, 60 + i * 30);
+
+                Controls.Add(lblSeat);
+            }
+        }
+
+        private async void btnCustomer_Click(object sender, EventArgs e)
+        {
+            await Task.Run(() => CustomerProcess());
+        }
+
+        private async void CustomerProcess()
+        {
+            await semaphore.WaitAsync(); // 顾客尝试获取信号量
+
+            if (queue.Count < 10) // 检查是否有空座位
+            {
+                int ticketNumber = queue.Count + 1;
+                queue.Enqueue(ticketNumber); // 领取号码
+
+                UpdateTicketDisplay(ticketNumber); // 更新界面显示
+                semaphore.Release(); // 释放信号量
+
+                await ServeCustomer(ticketNumber); // 等待顾客被叫号并服务
+            }
+            else
+            {
+                semaphore.Release(); // 释放信号量
+                MessageBox.Show("座位已满，请稍后再来！");
+            }
+        }
+
+        private async Task ServeCustomer(int ticketNumber)
+        {
+            await semaphore.WaitAsync(); // 营业员尝试获取信号量
+
+            // 模拟营业员服务顾客
+            await Task.Delay(1000); // 假设服务时间为2秒
+
+            UpdateServingDisplay(ticketNumber); // 更新界面显示
+            queue.Dequeue(); // 顾客被服务完毕，从队列中移除
+
+            semaphore.Release(); // 释放信号量
+        }
+
+        private void UpdateTicketDisplay(int ticketNumber)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action<int>(UpdateTicketDisplay), ticketNumber);
+            }
+            else
+            {
+                lblTicket.Text = "当前号码：" + ticketNumber;
+            }
+
+        }
+
+        private void UpdateServingDisplay(int ticketNumber)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action<int>(UpdateServingDisplay), ticketNumber);
+            }
+            else
+            {
+                lblServing.Text = "正在服务：" + ticketNumber;
+            }
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
