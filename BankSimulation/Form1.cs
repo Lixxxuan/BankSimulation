65970bd4cf020a025bb5f86fbce0fdc918b9b74d
Lixxxuan <1366070554@qq.com>
2023-12-12 14:29:44 +0800

    111

--- BankSimulation/Form1.cs
+++ BankSimulation/Form1.cs
@@ -8,6 +8,7 @@ namespace BankSimulation
 {
     public partial class Form1 : Form
     {
+        private List<CheckBox> seatCheckBoxes;
         private SemaphoreSlim semaphore; // 信号量，用于控制顾客的访问
         private Queue<int> queue; // 顾客队列，存储顾客的号码
         public Form1()
@@ -34,17 +35,24 @@ namespace BankSimulation
             Controls.Add(lblTicket);
             Controls.Add(lblServiceWindow);
         }
+        private List<bool> seatStatus;
 
         private void CreateSeats()
         {
+            seatStatus = new List<bool>();
+            seatCheckBoxes = new List<CheckBox>();
+
             for (int i = 0; i < 10; i++)
             {
-                Label lblSeat = new Label();
-                lblSeat.Text = "座位 " + (i + 1);
-                lblSeat.AutoSize = true;
-                lblSeat.Location = new System.Drawing.Point(20, 60 + i * 30);
+                CheckBox chkSeat = new CheckBox();
+                chkSeat.Text = "座位 " + (i + 1);
+                chkSeat.AutoSize = true;
+                chkSeat.Location = new System.Drawing.Point(20, 60 + i * 30);
 
-                Controls.Add(lblSeat);
+                seatCheckBoxes.Add(chkSeat); // 将 CheckBox 引用添加到列表中
+                Controls.Add(chkSeat);
+
+                seatStatus.Add(true); // 初始化座位状态为 true，表示空闲
             }
         }
 
@@ -61,6 +69,14 @@ namespace BankSimulation
             {
                 int ticketNumber = queue.Count + 1;
                 queue.Enqueue(ticketNumber); // 领取号码
+
+                // 占用座位
+                int seatIndex = ticketNumber - 1; // 座位索引从0开始
+                if (seatIndex >= 0 && seatIndex < seatStatus.Count)
+                {
+                    seatStatus[seatIndex] = false; // 将座位状态设置为非空闲
+                    UpdateSeatStatusDisplay(); // 更新座位状态显示
+                }
 
                 UpdateTicketDisplay(ticketNumber); // 更新界面显示
                 semaphore.Release(); // 释放信号量
@@ -84,6 +100,14 @@ namespace BankSimulation
             UpdateServingDisplay(ticketNumber); // 更新界面显示
             queue.Dequeue(); // 顾客被服务完毕，从队列中移除
 
+            // 释放座位
+            int seatIndex = ticketNumber - 1; // 座位索引从0开始
+            if (seatIndex >= 0 && seatIndex < seatStatus.Count)
+            {
+                seatStatus[seatIndex] = true; // 将座位状态设置为空闲
+                UpdateSeatStatusDisplay(); // 更新座位状态显示
+            }
+
             semaphore.Release(); // 释放信号量
         }
 
@@ -99,7 +123,21 @@ namespace BankSimulation
             }
 
         }
-
+        private void UpdateSeatStatusDisplay()
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action(UpdateSeatStatusDisplay));
+            }
+            else
+            {
+                for (int i = 0; i < seatStatus.Count; i++)
+                {
+                    CheckBox chkSeat = seatCheckBoxes[i];
+                    chkSeat.Checked = !seatStatus[i]; // 非空闲状态设置为选中
+                }
+            }
+        }
         private void UpdateServingDisplay(int ticketNumber)
         {
             if (InvokeRequired)
@@ -109,6 +147,14 @@ namespace BankSimulation
             else
             {
                 lblServing.Text = "正在服务：" + ticketNumber;
+
+                // 更新座位状态
+                int seatIndex = ticketNumber - 1; // 座位索引从0开始
+                if (seatIndex >= 0 && seatIndex < seatStatus.Count)
+                {
+                    seatStatus[seatIndex] = false; // 将座位状态设置为非空闲
+                    UpdateSeatStatusDisplay(); // 更新座位状态显示
+                }
             }
         }
 
